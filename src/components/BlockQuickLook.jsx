38be8002704f18f0545a4a56dff79662ee3d10e6
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-02 15:50:58 -0500

    Update quicklook design

--- src/components/BlockQuickLook.jsx
+++ src/components/BlockQuickLook.jsx
@@ -1,37 +1,40 @@
 import { MosaicContext } from './MosaicContext'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import { useHotkeys } from 'react-hotkeys-hook'
+import Spinner from './Spinner'
 
 function BlockQuickLook ({ blockData }) {
   const mosaic = useContext(MosaicContext)
+  const [imageLoaded, setImageLoaded] = useState(null)
 
-  const handleKeyDown = event => {
-    console.log(event)
-    if (event.key === 'Escape') {
-      close()
-    }
-  }
+  useHotkeys('esc', () => close(), { enabled: blockData !== null })
 
   const close = () => {
     mosaic.setQuickLookBlockData(null)
+    setImageLoaded(false)
+  }
+
+  const handleImageLoaded = () => {
+    setImageLoaded(true)
   }
 
   return (
-    <div
-      onKeyDown={handleKeyDown}
-      className='fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-50'
-    >
-      <div className='border bg-white rounded drop-shadow-xl'>
-        <div className='absolute -top-2 -right-2'>
-          <button className='p-4 text-xl' onClick={close}>X</button>
-        </div>
+    <div className='fixed top-0 left-0 h-screen w-screen z-50 p-6 cursor-pointer' onClick={close}>
+      <div className='h-full w-full flex justify-center items-center z-50 bg-black bg-opacity-80 backdrop-blur-sm border-black border-2 rounded-md shadow-md'>
         <div className='p-8 text-white'>
+          {!imageLoaded && <Spinner />}
+
           {blockData.image && (
-            <img src={blockData.image.display.url} className='aspect-square object-contain max-w-full' />
+            <img
+              src={blockData.image.display.url}
+              className='flex-1 aspect-square object-contain max-w-full max-h-full'
+              onLoad={handleImageLoaded}
+              hidden={!imageLoaded}
+            />
           )}
-          {blockData.title}
         </div>
       </div>
-    </div>
+    </div> 
   )
 }
 
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner () {
   return (
     <svg
-      className='animate-spin -ml-1 mr-3 h-8 w-8 text-black'
+      className='animate-spin -ml-1 mr-3 h-12 w-12'
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
       viewBox='0 0 24 24'
