daaad61c25039e04ce613b7ce499325e168d00c2
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-03 18:07:55 -0500

    Remove blueprint theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import '@blueprintjs/core/lib/css/blueprint.css'
-import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import 'react-mosaic-component/react-mosaic-component.css'
 import './index.css'
 import Desktop from './components/Desktop'
