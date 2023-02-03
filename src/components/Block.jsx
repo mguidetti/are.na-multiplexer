17d2db3a99a6f81e00af1a6f4f0f2ac4d1e648c7
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-03 16:00:45 -0500

    Add block preview components

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 import { useDrag } from 'react-dnd'
 import { MosaicContext } from './MosaicContext'
 
@@ -27,17 +27,63 @@ function Block ({ data, removeBlock }) {
     mosaic.setQuickLookBlockData(data)
   }
 
+  const renderBlock = () => {
+    switch (data.class) {
+      case 'Attachment':
+        return <AttachmentBlock data={data} />
+      case 'Image':
+        return <ImageBlock data={data} />
+      case 'Link':
+        return <ImageBlock data={data} />
+      case 'Text':
+        return <TextBlock data={data} />
+      default:
+        return <DefaultBlock data={data} />
+    }
+  }
+
   return (
     <div
       ref={drag}
       onDoubleClick={handleDoubleClick}
       className={classNames(
-        'border-2 text-white border-transparent hover:border-black aspect-square w-full h-full flex flex-col justify-center items-center cursor-pointer',
-        { 'opacity-10': isDragging }
+        'border text-primary border-transparent hover:border-secondary aspect-square w-full h-full flex flex-col justify-center items-center cursor-pointer',
+        { 'opacity-25': isDragging }
       )}
     >
-      {data.image && <img src={data.image.thumb.url} className='aspect-square object-contain w-full h-full p-0' />}
-      {!data.image && <p className='text-center text-xs pt-2'>{data.title}</p>}
+      {renderBlock(data)}
+    </div>
+  )
+}
+
+function ImageBlock ({ data }) {
+  return (
+    <React.Fragment>
+      <img src={data.image.thumb.url} className='aspect-square object-contain w-full h-full p-0' />
+    </React.Fragment>
+  )
+}
+
+function TextBlock ({ data }) {
+  return (
+    <div className='border border-primary/25 p-2 h-full w-full overflow-hidden'>
+      <p className='text-xs'>{data.content}</p>
+    </div>
+  )
+}
+
+function AttachmentBlock ({ data }) {
+  return (
+    <div className='h-full w-full bg-primary/10 flex items-center justify-center'>
+      <p className='text-center text-xs'>{data.title}</p>
+    </div>
+  )
+}
+
+function DefaultBlock ({ data }) {
+  return (
+    <div className='h-full w-full flex items-center justify-center'>
+      <p className='text-center text-xs'>{data.title}</p>
     </div>
   )
 }
