3d6463eb6e7f6231b24ffde7d5a99ff9d0614a8a
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-04 13:09:32 -0500

    Add React-select for channel loader

--- src/components/ChannelLoader.jsx
+++ src/components/ChannelLoader.jsx
@@ -1,121 +1,75 @@
 import classNames from 'classnames/bind'
-import { useCallback, useContext, useState, useRef } from 'react'
-import { DesktopContext } from './DesktopContext'
+import { useCallback, useContext, useRef, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
+import AsyncSelect from 'react-select/async'
 import { useArena } from '../hooks/useArena'
+import { DesktopContext } from './DesktopContext'
 
 function ChannelLoader () {
   const desktop = useContext(DesktopContext)
   const arena = useArena()
+  const select = useRef(null)
 
   const [isOpen, setIsOpen] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [channels, setChannels] = useState([])
-  const [selectedChannel, setSelectedChannel] = useState('')
-
-  const searchInputRef = useRef(null)
 
   useHotkeys('shift+l', () => open(), { enabled: !isOpen })
   useHotkeys('esc', () => close(), { enabled: isOpen })
 
   const open = () => {
     setIsOpen(true)
-
-    setTimeout(() => searchInputRef.current.focus(), 50)
-  }
-
-  const reset = () => {
-    setChannels([])
+    setTimeout(() => select.current.focus(), 50)
   }
 
   const close = () => {
     setIsOpen(false)
-    reset()
   }
 
-  const searchChannels = useCallback(
-    async event => {
-      reset()
-      setIsLoading(true)
+  const loadChannels = useCallback(async query => {
+    const results = await arena.search(query).channels({ page: 1, per: 10 })
 
-      const query = event.target.value
-      const results = await arena.search(query).channels({ page: 1, per: 10 })
+    return results
+  })
 
-      setIsLoading(false)
-
-      if (results.length) {
-        setChannels(results)
-        setSelectedChannel(results[0].id)
-      } else {
-        console.log('no results')
-      }
-    },
-    [arena]
-  )
-
-  const handleSelectChange = event => {
-    setSelectedChannel(event.target.value)
-  }
-
-  const handleLoad = () => {
-    desktop.setNewTileChannelId(selectedChannel)
+  const handleSelectChange = channel => {
+    desktop.setNewTileChannelId(channel.id)
     desktop.addToTopRight()
     close()
   }
 
-  const dialogClasses = classNames(
-    'fixed border-2 bg-background bg-opacity-80 backdrop-blur-md rounded-md p-4 z-30 bg-background mx-auto top-12 left-0 right-0 w-1/2 shadow-xl',
-    { hidden: !isOpen }
-  )
+  const dialogClasses = classNames('fixed z-30 mx-auto top-12 left-0 right-0 w-1/2', { hidden: !isOpen })
 
   return (
     <div className='relative'>
       <button
-        className={classNames('border py-1 px-4 rounded-lg text-primary hover:bg-secondary/50', {
-          'bg-secondary/50': isOpen
-        })}
+        className={classNames(
+          'border-2 border-t-0 border-primary/70 py-1 px-4 rounded-b-lg text-primary/70 hover:bg-secondary/50 hover:border-secondary transition-all',
+          {
+            'bg-secondary/75 pt-2 4 hover:bg-secondary/75': isOpen
+          }
+        )}
         onClick={open}
       >
         Load Channel
       </button>
 
       <div className={dialogClasses}>
-        <div className='flex flex-col gap-y-2'>
-          <input
-            type='text'
-            ref={searchInputRef}
-            onChange={searchChannels}
-            className='border border-inherit rounded p-2 bg-transparent'
-            placeholder='Search channels'
-          />
-          <select
-            name='channels-list'
-            id='channels-list'
-            className='border rounded p-2 bg-background appearance-none'
-            onChange={handleSelectChange}
-          >
-            {isLoading && (
-              <option selected disabled>
-                Loading...
-              </option>
-            )}
-            {channels &&
-              channels.map(channel => (
-                <option key={channel.id} value={channel.id}>
-                  {channel.title}
-                </option>
-              ))}
-          </select>
-        </div>
-
-        <div className='flex gap-x-2 mt-4'>
-          <button className='border p-2 rounded hover:bg-secondary/50' onClick={handleLoad}>
-            Load into new window
-          </button>
-          <button className='border p-2 rounded hover:bg-secondary/50' onClick={close}>
-            Cancel
-          </button>
-        </div>
+        <AsyncSelect
+          ref={select}
+          cacheOptions
+          defaultOptions={[]}
+          components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
+          getOptionLabel={e => e.title}
+          getOptionValue={e => e.id}
+          loadOptions={loadChannels}
+          onChange={handleSelectChange}
+          unstyled
+          classNames={{
+            container: () => 'bg-background bg-opacity-80 backdrop-blur-md border-2 rounded-md px-2',
+            menu: () =>
+              'bg-background bg-opacity-80 backdrop-blur-md border-2 border-t-0 rounded-b-md thin-scrollbar',
+            option: state => (state.isFocused ? 'py-1 px-2 bg-secondary/75 text-white' : 'py-1 px-2')
+          }}
+        />
       </div>
     </div>
   )
