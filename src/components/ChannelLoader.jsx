b4fac4bf7a6f1e15ac9a5fc9fb3fc90dd979d7e1
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-04 14:10:59 -0500

    Simplify and style channel loader

--- src/components/ChannelLoader.jsx
+++ src/components/ChannelLoader.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind'
-import { useCallback, useContext, useRef, useState } from 'react'
+import { useCallback, useContext, useRef } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 import AsyncSelect from 'react-select/async'
 import { useArena } from '../hooks/useArena'
@@ -10,19 +10,7 @@ function ChannelLoader () {
   const arena = useArena()
   const select = useRef(null)
 
-  const [isOpen, setIsOpen] = useState(false)
-
-  useHotkeys('shift+l', () => open(), { enabled: !isOpen })
-  useHotkeys('esc', () => close(), { enabled: isOpen })
-
-  const open = () => {
-    setIsOpen(true)
-    setTimeout(() => select.current.focus(), 50)
-  }
-
-  const close = () => {
-    setIsOpen(false)
-  }
+  useHotkeys('shift+l', () => select.current.focus())
 
   const loadChannels = useCallback(async query => {
     const results = await arena.search(query).channels({ page: 1, per: 10 })
@@ -33,43 +21,31 @@ function ChannelLoader () {
   const handleSelectChange = channel => {
     desktop.setNewTileChannel(channel)
     desktop.addToTopRight()
-    close()
   }
 
-  const dialogClasses = classNames('fixed z-30 mx-auto top-12 left-0 right-0 w-1/2', { hidden: !isOpen })
-
   return (
-    <div className='relative'>
-      <button
-        className={classNames(
-          'border-2 border-t-0 border-primary/70 py-1 px-4 rounded-b-lg text-primary/70 hover:bg-secondary/50 hover:border-secondary transition-all',
-          {
-            'bg-secondary/75 pt-2 4 hover:bg-secondary/75': isOpen
-          }
-        )}
-        onClick={open}
-      >
-        Load Channel
-      </button>
-
-      <div className={dialogClasses}>
-        <AsyncSelect
-          ref={select}
-          cacheOptions
-          defaultOptions={[]}
-          components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
-          getOptionLabel={e => e.title}
-          getOptionValue={e => e.id}
-          loadOptions={loadChannels}
-          onChange={handleSelectChange}
-          unstyled
-          classNames={{
-            container: () => 'bg-background bg-opacity-80 backdrop-blur-md border-2 rounded-md px-2',
-            menu: () => 'bg-background bg-opacity-80 backdrop-blur-md border-2 border-t-0 rounded-b-md thin-scrollbar',
-            option: state => (state.isFocused ? 'py-1 px-2 bg-secondary/75 text-white' : 'py-1 px-2')
-          }}
-        />
-      </div>
+    <div className='p-2 w-[400px] z-30'>
+      <AsyncSelect
+        ref={select}
+        cacheOptions
+        blurInputOnSelect={true}
+        placeholder={'Load channel'}
+        components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
+        getOptionLabel={e => ` ${e.user.full_name} / ${e.title}`}
+        getOptionValue={e => e.id}
+        loadOptions={loadChannels}
+        onChange={handleSelectChange}
+        unstyled
+        classNames={{
+          container: state =>
+            classNames('bg-background border-2 border-primary/70 rounded-md px-2', {
+              'border-secondary': state.isFocused
+            }),
+          menu: () => 'left-0 bg-background border-2 border-primary/70 border-t-background rounded-b-md',
+          menuList: () => 'scrollbar-thin scrollbar-thumb-primary/70 scrollbar-track-primary/25',
+          option: state => classNames('py-1 px-2', { 'bg-secondary/50 text-white': state.isFocused })
+        }}
+      />
     </div>
   )
 }
