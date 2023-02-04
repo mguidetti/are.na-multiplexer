445458d3f61a72138e17f34ed7646022efe4f9e2
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-04 14:19:08 -0500

    Style header

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Are.na Shelf OS</title>
+        <title>Are.na Shelf</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
