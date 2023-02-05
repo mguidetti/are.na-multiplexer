946f5f46a6b1f766a4801daa98a917c409c710e5
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-05 15:19:49 -0500

    Fix double multibackend bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,19 @@
 import { SessionProvider } from 'next-auth/react'
 import 'react-mosaic-component/react-mosaic-component.css'
 import '../styles/globals.css'
+import { DndProvider } from 'react-dnd-multi-backend'
+import HTML5toTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
 
-export default function App ({ Component, pageProps: {session, ...pageProps} }) {
+export default function App ({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {/* 
+        DndProvider included to fix https://github.com/nomcopter/react-mosaic/issues/162    
+        Fix from https://github.com/nomcopter/react-mosaic/issues/162#issuecomment-1194558777 *
+      */}
+      <DndProvider options={HTML5toTouch}>
+        <Component {...pageProps} />
+      </DndProvider>
     </SessionProvider>
   )
 }
