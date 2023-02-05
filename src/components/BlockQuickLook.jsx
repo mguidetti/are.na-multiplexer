1b893a7b1ce5ed9b80e1a96c349cd2a1750ab1ca
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-05 16:41:11 -0500

    Build out quick look block types

--- src/components/BlockQuickLook.jsx
+++ src/components/BlockQuickLook.jsx
@@ -2,6 +2,9 @@ import { DesktopContext } from './DesktopContext'
 import { useContext, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 import Spinner from './Spinner'
+import XMarkIcon from '@/icons/x-mark.svg'
+import ArenaMarkIcon from '@/icons/arena-mark.svg'
+import dayjs from 'dayjs'
 
 function BlockQuickLook ({ blockData }) {
   const desktop = useContext(DesktopContext)
@@ -18,25 +21,91 @@ function BlockQuickLook ({ blockData }) {
     setImageLoaded(true)
   }
 
-  return (
-    <div className='fixed top-0 left-0 h-screen w-screen z-50 p-8 cursor-pointer' onClick={close}>
-      <div className='relative h-full w-full overflow-hidden flex justify-center items-center z-50 bg-background bg-opacity-80 backdrop-blur-md border-2 border-secondary rounded-sm'>
+  function AttachmentBlock ({ data }) {
+    return (
+      <>
+        <ImageBlock data={data} />
+      </>
+    )
+  }
+
+  function ImageBlock ({ data }) {
+    return (
+      <>
         {!imageLoaded && <Spinner />}
+        <img
+          src={data.image?.display?.url}
+          className='aspect-square object-contain w-full h-full'
+          onLoad={handleImageLoaded}
+          hidden={!imageLoaded}
+        />
+      </>
+    )
+  }
 
-        {blockData.image && (
-          <img
-            src={blockData.image.display.url}
-            className=' aspect-square object-contain max-h-full max-w-full p-8'
-            onLoad={handleImageLoaded}
-            hidden={!imageLoaded}
-          />
-        )}
+  function LinkBlock ({ data }) {
+    return (
+      <>
+        <a href={data.source.url} target='_blank'>
+          <ImageBlock data={data} />
+        </a>
+      </>
+    )
+  }
 
-        <div className="absolute top-0 right-0 text-primary border-secondary border-2 m-2 p-4 rounded-sm bg-background bg-opacity-80 backdrop-blur-md text-right">
-          <div className="text-xl">{blockData.generated_title}</div>
-          <div className="mt-2 text-sm">{blockData.connected_at}</div>
+  function MediaBlock ({ data }) {
+    return (
+      <>
+        <a href={data.source.url} target='_blank'>
+          <ImageBlock data={data} />
+        </a>
+      </>
+    )
+  }
+
+  function TextBlock ({ data }) {
+    return (
+      <>
+        <p className='text-base self-start justify-self-start'>{data.content}</p>
+      </>
+    )
+  }
+
+  const renderBlock = () => {
+    switch (blockData.class) {
+      case 'Attachment':
+        return <AttachmentBlock data={blockData} />
+      case 'Image':
+        return <ImageBlock data={blockData} />
+      case 'Media':
+        return <MediaBlock data={blockData} />
+      case 'Link':
+        return <LinkBlock data={blockData} />
+      case 'Text':
+        return <TextBlock data={blockData} />
+    }
+  }
+
+  return (
+    <div className='fixed top-0 left-0 h-screen w-screen z-50 p-8'>
+      <div className='relative h-full w-full overflow-hidden flex justify-center items-center z-50 bg-background bg-opacity-80 backdrop-blur-md border-2 border-secondary rounded-sm p-8'>
+        {renderBlock()}
+
+        <div className='absolute bottom-0 right-0 text-primary text-sm border-secondary border-t-2 border-l-2 py-2 px-4 rounded-sm bg-background bg-opacity-80 backdrop-blur-md text-right'>
+          <h1 className='font-bold'>{blockData.generated_title}</h1>
+          <p className=''>Connected {dayjs(blockData.connected_at).format("MMM D, YYYY HH:MM:ss ZZ")} by {blockData.user.full_name}</p>
+          {blockData.source && <a href={blockData.source.url} target="_blank" className="underline">{blockData.source.url}</a>}
+          {blockData.description && <p>{blockData.description}</p>}
         </div>
+
+        <button onClick={close} className='fixed top-0 right-0'>
+          <XMarkIcon className='w-8 hover:text-secondary' strokeWidth='1.5' />
+        </button>
+        <a href={`https://are.na/block/${blockData.id}`} target='_blank' className='fixed bottom-0 left-0 p-2'>
+          <ArenaMarkIcon className="w-8 hover:text-secondary" />
+        </a>
       </div>
+      <div onClick={close} className='absolute inset-0 cursor-pointer'></div>
     </div>
   )
 }
--- src/components/GridBlock.jsx
+++ src/components/GridBlock.jsx
@@ -2,11 +2,7 @@ import Block from './Block'
 
 function GridBlock ({ data, disconnectBlock }) {
   function AttachmentBlock ({ data }) {
-    return (
-      <div className='h-full w-full bg-primary/10 flex items-center justify-center'>
-        <p className='text-center text-xs'>{data.title}</p>
-      </div>
-    )
+    return <ImageBlock data={data} />
   }
 
   function ChannelBlock ({ data }) {
@@ -22,7 +18,7 @@ function GridBlock ({ data, disconnectBlock }) {
   function ImageBlock ({ data }) {
     return (
       <>
-        <img src={data.image.thumb.url} className='aspect-square object-contain w-full h-full p-0' />
+        <img src={data.image?.thumb?.url} className='aspect-square object-contain w-full h-full p-0' />
       </>
     )
   }
@@ -31,14 +27,6 @@ function GridBlock ({ data, disconnectBlock }) {
     return (
       <div className='border border-primary/25 p-2 h-full w-full overflow-hidden'>
         <p className='text-xs-relative'>{data.content}</p>
-      </div>
-    )
-  }
-
-  function DefaultBlock ({ data }) {
-    return (
-      <div className='h-full w-full flex items-center justify-center p-2'>
-        <p className='text-center text-xs-relative'>{data.title}</p>
       </div>
     )
   }
@@ -57,8 +45,6 @@ function GridBlock ({ data, disconnectBlock }) {
         return <ImageBlock data={data} />
       case 'Text':
         return <TextBlock data={data} />
-      default:
-        return <DefaultBlock data={data} />
     }
   }
 
