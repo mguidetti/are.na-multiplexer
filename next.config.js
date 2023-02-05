e0575b29f96902db43031876eaf2290308fc46fa
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-05 11:26:06 -0500

    Use SVGR for svg icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: false
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
+  }
+}
--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -5,6 +5,12 @@ import { MosaicContext, MosaicWindow } from 'react-mosaic-component'
 import { useArena } from '../hooks/useArena'
 import Block from './Block'
 import Spinner from './Spinner'
+import XMarkIcon from '@/icons/x-mark.svg'
+import PlusIcon from '@/icons/plus.svg'
+import MinusIcon from '@/icons/minus.svg'
+import ArrowsPointingOutIcon from '@/icons/arrows-pointing-out.svg'
+import ListBulletIcon from '@/icons/list-bullet.svg'
+import Squares2x2Icon from '@/icons/squares-2x2.svg'
 
 function Window ({ path, channelData }) {
   const mosaic = useContext(MosaicContext)
@@ -182,60 +188,24 @@ function Window ({ path, channelData }) {
     return (
       <div className='flex justify-end'>
         <button onClick={toggleView} className='hover:text-secondary px-1'>
-          {view === 'list' && (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              class='w-5 h-5'
-            >
-              <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z'
-              />
-            </svg>
-          )}
-
-          {view === 'grid' && (
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='w-5 h-5'>
-              <path
-                fill-rule='evenodd'
-                d='M6 4.75A.75.75 0 016.75 4h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 4.75zM6 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 10zm0 5.25a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75a.75.75 0 01-.75-.75zM1.99 4.75a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 15.25a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 10a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1V10z'
-                clip-rule='evenodd'
-              />
-            </svg>
-          )}
+          {view === 'list' && (<Squares2x2Icon className='w-5 h-5' />)}
+          {view === 'grid' && (<ListBulletIcon className='w-5 h-5' />)}
         </button>
 
         <button onClick={incrementGrid} className='hover:text-secondary px-1'>
-          <svg fill='none' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' className='w-5 h-5'>
-            <path d='M12 4.5v15m7.5-7.5h-15' />
-          </svg>
+          <PlusIcon className='w-5 h-5' strokeWidth='2' />
         </button>
 
         <button onClick={decrementGrid} className='hover:text-secondary px-1'>
-          <svg fill='none' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' className='w-5 h-5'>
-            <path d='M19.5 12h-15' />
-          </svg>
+          <MinusIcon className='w-5 h-5' strokeWidth='2' />
         </button>
 
         <button onClick={expand} className='hover:text-secondary px-1'>
-          <svg fill='none' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' className='w-5 h-5'>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
-            />
-          </svg>
+          <ArrowsPointingOutIcon className='w-5 h-5' />
         </button>
 
         <button onClick={remove} className='hover:text-secondary px-1'>
-          <svg fill='none' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' className='w-5 h-5'>
-            <path d='M6 18L18 6M6 6l12 12' />
-          </svg>
+          <XMarkIcon className='w-5 h-5' strokeWidth='2' />
         </button>
       </div>
     )
