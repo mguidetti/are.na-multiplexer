bd8fb90f1514f52a456ec26f790f3d3c71fc144d
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-05 12:06:37 -0500

    Style header text and rename

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,14 @@ function Header () {
 
   return (
     <div className='grid grid-cols-3 gap-x-4 items-start bg-background text-primary/70'>
-      <div className='flex items-center gap-x-2 p-2'>
+      <div className='flex items-center gap-x-2 p-3'>
         <ArenaMark className='w-8' />
-        <span className='font-mono text-sm'>SHELF</span>
+        <span>A.MUX</span>
       </div>
       <div className='flex-1 flex justify-center'>
         <ChannelLoader />
       </div>
-      <div className='text-right flex gap-x-2 justify-end items-center p-1 font-mono text-sm'>
+      <div className='text-right flex gap-x-2 justify-end items-center p-3'>
         {data?.user?.name}
         <button onClick={() => signOut()} className='p-1 hover:text-secondary' title='Sign out'>
           <SignOutIcon className='w-5' />
