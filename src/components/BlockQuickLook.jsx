e4d6f1a9f7403f57a194fc8d57a0c1fecd0383bc
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-06 17:10:04 -0500

    Style quicklook background

--- src/components/BlockQuickLook.jsx
+++ src/components/BlockQuickLook.jsx
@@ -87,11 +87,11 @@ function BlockQuickLook ({ blockData }) {
   }
 
   return (
-    <div className='fixed top-0 left-0 h-screen w-screen z-50 p-8'>
-      <div className='relative h-full w-full overflow-hidden flex justify-center items-center z-50 bg-background bg-opacity-80 backdrop-blur-md border-2 border-secondary rounded-sm p-8'>
+    <div className='fixed top-0 left-0 h-screen w-screen z-50 p-8 bg-background/30'>
+    <div className='relative h-full w-full overflow-hidden flex justify-center items-center z-50 bg-background bg-opacity-70  border-2 border-secondary rounded-sm p-8 drop-shadow-panel'>
         {renderBlock()}
 
-        <div className='absolute bottom-0 right-0 text-primary text-sm border-secondary border-t-2 border-l-2 py-2 px-4 rounded-sm bg-background bg-opacity-80 backdrop-blur-md text-right'>
+        <div className='absolute bottom-0 right-0 text-primary text-sm border-secondary border-t-2 border-l-2 py-2 px-4 rounded-sm bg-background bg-opacity-90 text-right'>
           <h1 className='font-bold'>{blockData.generated_title}</h1>
           <p className=''>Connected {dayjs(blockData.connected_at).format("MMM D, YYYY HH:MM:ss ZZ")} by {blockData.user.full_name}</p>
           {blockData.source && <a href={blockData.source.url} target="_blank" className="underline">{blockData.source.url}</a>}
