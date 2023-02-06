565e751de553505e38e266b5e3611bc067cf0fc1
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-05 20:51:18 -0500

    Refactor ListBlock to render channels correctly

--- src/components/ListBlock.jsx
+++ src/components/ListBlock.jsx
@@ -1,15 +1,37 @@
 import Block from './Block'
+import SquareIcon from '@/icons/square.svg'
 
 function ListBlock ({ data, disconnectBlock }) {
-  return (
-      <Block data={data} disconnectBlock={disconnectBlock}>
-        <div className='grid grid-cols-[min-content_1fr] gap-x-4 items-center py-1 text-md-relative hover:bg-secondary/30 cursor-pointer'>
-          <div className='w-[calc(1.5em*var(--scale))]'>
-            {data.image && <img src={data.image.thumb.url} className='aspect-square object-contain' />}
-          </div>
-          <div className='truncate'>{data.generated_title}</div>
+  function ChannelBody () {
+    return (
+      <>
+        <div className='w-[calc(1.5em*var(--scale))] flex items-center justify-center channel-block'>
+          <SquareIcon className='w-6 h-6' strokeWidth="1" />
         </div>
-      </Block>
+        <div className='truncate channel-block'>{`${data.user.full_name} / ${data.title}`}</div>
+      </>
+    )
+  }
+
+  function BlockBody () {
+    return (
+      <>
+        <div className='w-[calc(1.5em*var(--scale))] flex items-center justify-center'>
+          {data.image && <img src={data.image.thumb.url} className='aspect-square object-contain' />}
+        </div>
+        <div className='truncate'>
+          {data.title || data.generated_titel}
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <Block data={data} disconnectBlock={disconnectBlock}>
+      <div className={`grid grid-cols-[min-content_1fr] gap-x-4 items-center py-1 text-md-relative hover:bg-secondary/30 cursor-pointer channel-status-${data.status}`}>
+        {data.class === 'Channel' ? <ChannelBody /> : <BlockBody />}
+      </div>
+    </Block>
   )
 }
 
