8ba04bde2a1e10fa087528928583616c69eb222a
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-07 18:33:37 -0500

    Update channel creator

--- src/components/ChannelCreator.jsx
+++ src/components/ChannelCreator.jsx
@@ -1,9 +1,12 @@
 import { useArena } from '@/hooks/useArena'
 import PlusIcon from '@/icons/plus.svg'
 import XMarkIcon from '@/icons/x-mark.svg'
-import { useCallback, useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 import { DesktopContext } from './DesktopContext'
+import classNames from 'classnames'
+import Spinner from './Spinner'
+import Select from 'react-select'
 
 function Dialog ({ close }) {
   const desktop = useContext(DesktopContext)
@@ -14,62 +17,90 @@ function Dialog ({ close }) {
     name: '',
     privacy: ''
   })
+  const [error, setError] = useState(null)
 
   const handleChange = event => {
     setFormData({ ...formData, [event.target.name]: event.target.value })
   }
 
-  const createChannel = useCallback(
-    async event => {
-      event.preventDefault()
+  const handleSelectChange = value => {
+    setFormData({ ...formData, privacy: value })
+  }
 
-      setIsCreating(true)
+  const createChannel = async event => {
+    event.preventDefault()
+    setIsCreating(true)
 
-      const results = await arena.channel(formData.name).create(formData.privacy)
+    try {
+      const results = await arena.channel(formData.name).create(formData.privacy.value)
 
       if (results) {
         desktop.loadChannel(results)
+        setFormData({})
+        close()
       }
-
+    } catch (error) {
+      console.log(error)
+      setError(error)
+    } finally {
       setIsCreating(false)
-      setFormData({})
-      close()
-    },
-    [arena]
-  )
+    }
+  }
+
+  const privacyOptions = [
+    { value: 'open', label: 'Open' },
+    { value: 'closed', label: 'Closed' },
+    { value: 'private', label: 'Private' }
+  ]
 
   return (
-    <div className='absolute top-10 left-0 z-50 border-2 border-secondary bg-zinc-900 p-8 rounded-md drop-shadow-panel'>
-      <button className='p-1 hover:text-secondary absolute top-0 right-0' onClick={close}>
+    <div className='absolute top-10 left-0 z-50 border-2 border-secondary bg-zinc-900 p-6 pt-4 rounded-md drop-shadow-panel'>
+      <button className='p-2 hover:text-secondary absolute top-0 right-0' onClick={close}>
         <XMarkIcon className='h-6 w-6 ' strokeWidth='2' />
       </button>
+      {isCreating && (
+        <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center z-10'>
+          <Spinner />
+        </div>
+      )}
 
-      <form className='flex-col flex space-y-4' onSubmit={createChannel}>
+      {error && <p className='text-red'>{error.message}</p>}
+
+      <form
+        className={classNames('flex-col flex space-y-3 font-bold', { invisible: isCreating })}
+        onSubmit={createChannel}
+      >
+        <h2>New Channel</h2>
+
         <input
           name='name'
           placeholder='Name'
-          className='bg-background px-2 py-1 border-primary/70 border rounded-md'
+          className='bg-background px-2 py-1 border-primary/70 border-2 rounded-md'
           value={formData.name}
           onChange={handleChange}
         />
 
-        <select
-          name='privacy'
-          className='bg-background px-2 py-1 border-primary/70 border rounded-md'
-          onChange={handleChange}
-        >
-          <option value='open' className='text-public-channel'>
-            Open
-          </option>
-          <option value='closed' className='text-primary'>
-            Closed
-          </option>
-          <option value='private' className='text-private-channel'>
-            Private
-          </option>
-        </select>
+        <Select
+          unstyled
+          options={privacyOptions}
+          value={formData.privacy}
+          onChange={handleSelectChange}
+          classNames={{
+            control: () => classNames('bg-background border-2 rounded-md border-primary/70 px-2 font-bold', {}),
+            menu: () => '-mt-1 bg-zinc-900 border-2 border-t-0 border-secondary rounded-b-md drop-shadow-panel',
+            menuList: () => 'scrollbar-thin scrollbar-thumb-secondary/50 scrollbar-track-secondary/30 rounded-b-md',
+            option: ({ value, isFocused }) =>
+              classNames('py-1 px-2 text-primary font-bold truncate', {
+                'bg-secondary/50': isFocused,
+                '!text-public-channel': value === 'open',
+                '!text-private-channel': value === 'private'
+              })
+          }}
+        />
 
-        <button className='px-2 py-1 border border-primary/70 rounded-md'>Create</button>
+        <button className='px-2 py-1 border-2 border-primary/70 rounded-md hover:text-secondary hover:border-secondary hover:bg-secondary/10'>
+          Create
+        </button>
       </form>
     </div>
   )
@@ -91,11 +122,13 @@ function ChannelCreator () {
   return (
     <div className='relative'>
       <button
-        className='border-2 rounded-md border-primary/70 px-2 py-1 font-bold bg-background min-h-[38px] hover:bg-secondary/10 hover:border-secondary group whitespace-nowrap flex gap-x-2 items-center'
+        className={classNames(
+          'border-2 rounded-md border-primary/70 px-2 py-1 font-bold bg-background min-h-[38px] hover:bg-secondary/10 hover:border-secondary hover:text-secondary group whitespace-nowrap flex gap-x-2 items-center',
+          { 'bg-secondary/10 !border-secondary text-secondary': isDialogOpen }
+        )}
         onClick={open}
       >
-        <PlusIcon className='w-5 h-5 text-primary/70 group-hover:text-secondary' strokeWidth='2' />
-        Create Channel
+        <PlusIcon className='w-5 h-5 text-inherit' strokeWidth='3' strokeLinecap='square' />
       </button>
 
       {isDialogOpen && <Dialog close={close} />}
