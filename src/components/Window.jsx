d90facd32902f896720d1e9591738dbfd175eff1
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-08 15:19:23 -0500

    Extract WindowToolbar

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,18 +1,11 @@
-import ArenaMarkIcon from '@/icons/arena-mark.svg'
-import ArrowsPointingOutIcon from '@/icons/arrows-pointing-out.svg'
-import ListBulletIcon from '@/icons/list-bullet.svg'
-import MinusIcon from '@/icons/minus.svg'
-import PlusIcon from '@/icons/plus.svg'
-import Squares2x2Icon from '@/icons/squares-2x2.svg'
-import XMarkIcon from '@/icons/x-mark.svg'
 import classNames from 'classnames/bind'
-import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useDrop } from 'react-dnd'
-import { MosaicContext, MosaicWindow, MosaicWindowContext } from 'react-mosaic-component'
+import { MosaicWindow } from 'react-mosaic-component'
 import { useArena } from '../hooks/useArena'
 import Spinner from './Spinner'
 import Blocks from './Blocks'
-import { DesktopContext } from './DesktopContext'
+import WindowToolbar from './WindowToolbar'
 
 function Window ({ path, channel }) {
   const arena = useArena()
@@ -21,14 +14,16 @@ function Window ({ path, channel }) {
   const [blocks, setBlocks] = useState([])
   const [page, setPage] = useState(1)
   const [error, setError] = useState(null)
-  const [scale, setscale] = useState(1)
+  const [scale, setScale] = useState(1)
   const [view, setView] = useState('grid')
 
   const blockPageSize = 50
   const totalPages = useMemo(() => Math.ceil(channel.length / blockPageSize), [channel])
-  const scaleMultiplier = 1.25
-  const minScale = 0.75
-  const maxScale = 3
+  const scaleDefaults = {
+    multiplier: 1.25,
+    min: 0.75,
+    max: 3
+  }
 
   useEffect(() => {
     fetchBlocks()
@@ -110,9 +105,16 @@ function Window ({ path, channel }) {
       title={`${channel.user.full_name} / ${channel.title}`}
       className={`channel-status-${channel.status}`}
       path={path}
-      toolbarControls={<ToolbarControls />}
-      onDragStart={() => console.log('MosaicWindow.onDragStart')}
-      onDragEnd={type => console.log('MosaicWindow.onDragEnd', type)}
+      toolbarControls={
+        <WindowToolbar
+          scaleDefaults={scaleDefaults}
+          scale={scale}
+          setScale={setScale}
+          view={view}
+          setView={setView}
+          channel={channel}
+        />
+      }
     >
       <div
         style={{ '--scale': scale }}
@@ -139,73 +141,6 @@ function Window ({ path, channel }) {
       </div>
     </MosaicWindow>
   )
-
-  function ToolbarControls () {
-    const mosaic = useContext(MosaicContext)
-    const mosaicWindow = useContext(MosaicWindowContext)
-    const desktop = useContext(DesktopContext)
-
-    const remove = () => {
-      mosaic.mosaicActions.remove(mosaicWindow.mosaicWindowActions.getPath())
-      desktop.removeChannel(channel.id)
-    }
-
-    const expand = () => {
-      mosaic.mosaicActions.expand(mosaicWindow.mosaicWindowActions.getPath(), 80)
-    }
-
-    const incrementScale = () => {
-      const value = scale * scaleMultiplier
-      const clampedValue = Math.min(Math.max(value, minScale), maxScale)
-
-      setscale(clampedValue)
-    }
-
-    const decrementScale = () => {
-      const value = scale / scaleMultiplier
-      const clampedValue = Math.min(Math.max(value, minScale), maxScale)
-
-      setscale(clampedValue)
-    }
-
-    const toggleView = () => {
-      setView(view === 'grid' ? 'list' : 'grid')
-    }
-
-    return (
-      <div className='flex justify-end items-center'>
-        <a
-          href={`https://www.are.na/${channel.owner_slug}/${channel.slug}`}
-          className='hover:text-secondary px-2'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <ArenaMarkIcon className='w-6' />
-        </a>
-
-        <button onClick={toggleView} className='hover:text-secondary px-1'>
-          {view === 'list' && <Squares2x2Icon className='w-5 h-5' />}
-          {view === 'grid' && <ListBulletIcon className='w-5 h-5' />}
-        </button>
-
-        <button onClick={incrementScale} className='hover:text-secondary px-1'>
-          <PlusIcon className='w-5 h-5' strokeWidth='2' />
-        </button>
-
-        <button onClick={decrementScale} className='hover:text-secondary px-1'>
-          <MinusIcon className='w-5 h-5' strokeWidth='2' />
-        </button>
-
-        <button onClick={expand} className='hover:text-secondary px-1'>
-          <ArrowsPointingOutIcon className='w-5 h-5' />
-        </button>
-
-        <button onClick={remove} className='hover:text-secondary px-1'>
-          <XMarkIcon className='w-5 h-5' strokeWidth='2' />
-        </button>
-      </div>
-    )
-  }
 }
 
 function BlankSlate () {
---
+++ src/components/WindowToolbar.jsx
@@ -0,0 +1,79 @@
+import ArenaMarkIcon from '@/icons/arena-mark.svg'
+import ArrowsPointingOutIcon from '@/icons/arrows-pointing-out.svg'
+import ListBulletIcon from '@/icons/list-bullet.svg'
+import MinusIcon from '@/icons/minus.svg'
+import PlusIcon from '@/icons/plus.svg'
+import Squares2x2Icon from '@/icons/squares-2x2.svg'
+import XMarkIcon from '@/icons/x-mark.svg'
+import { useContext } from 'react'
+import { MosaicContext, MosaicWindowContext } from 'react-mosaic-component'
+import { DesktopContext } from './DesktopContext'
+
+function WindowToolbar ({ scale, setScale, scaleDefaults, setView, view, channel }) {
+  const mosaic = useContext(MosaicContext)
+  const mosaicWindow = useContext(MosaicWindowContext)
+  const desktop = useContext(DesktopContext)
+
+  const remove = () => {
+    mosaic.mosaicActions.remove(mosaicWindow.mosaicWindowActions.getPath())
+    desktop.removeChannel(channel.id)
+  }
+
+  const expand = () => {
+    mosaic.mosaicActions.expand(mosaicWindow.mosaicWindowActions.getPath(), 80)
+  }
+
+  const incrementScale = () => {
+    const value = scale * scaleDefaults.multiplier
+    const clampedValue = Math.min(Math.max(value, scaleDefaults.min), scaleDefaults.max)
+
+    setScale(clampedValue)
+  }
+
+  const decrementScale = () => {
+    const value = scale / scaleDefaults.multiplier
+    const clampedValue = Math.min(Math.max(value, scaleDefaults.min), scaleDefaults.max)
+
+    setScale(clampedValue)
+  }
+
+  const toggleView = () => {
+    setView(view === 'grid' ? 'list' : 'grid')
+  }
+
+  return (
+    <div className='flex justify-end items-center'>
+      <a
+        href={`https://www.are.na/${channel.owner_slug}/${channel.slug}`}
+        className='hover:text-secondary px-2'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <ArenaMarkIcon className='w-6' />
+      </a>
+
+      <button onClick={toggleView} className='hover:text-secondary px-1'>
+        {view === 'list' && <Squares2x2Icon className='w-5 h-5' />}
+        {view === 'grid' && <ListBulletIcon className='w-5 h-5' />}
+      </button>
+
+      <button onClick={incrementScale} className='hover:text-secondary px-1'>
+        <PlusIcon className='w-5 h-5' strokeWidth='2' />
+      </button>
+
+      <button onClick={decrementScale} className='hover:text-secondary px-1'>
+        <MinusIcon className='w-5 h-5' strokeWidth='2' />
+      </button>
+
+      <button onClick={expand} className='hover:text-secondary px-1'>
+        <ArrowsPointingOutIcon className='w-5 h-5' />
+      </button>
+
+      <button onClick={remove} className='hover:text-secondary px-1'>
+        <XMarkIcon className='w-5 h-5' strokeWidth='2' />
+      </button>
+    </div>
+  )
+}
+
+export default WindowToolbar
