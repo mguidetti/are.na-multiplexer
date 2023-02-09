14c71bd90b254a6334a275e08f8491def15e7bf5
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-08 21:33:10 -0500

    Add vercel audience analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,23 @@
+import { Analytics } from '@vercel/analytics/react'
 import { SessionProvider } from 'next-auth/react'
-import 'react-mosaic-component/react-mosaic-component.css'
-import '../styles/globals.css'
 import { DndProvider } from 'react-dnd-multi-backend'
 import HTML5toTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
+import 'react-mosaic-component/react-mosaic-component.css'
+import '../styles/globals.css'
 
 export default function App ({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      {/*
+    <>
+      <SessionProvider session={session}>
+        {/*
         DndProvider included to fix https://github.com/nomcopter/react-mosaic/issues/162
         Fix from https://github.com/nomcopter/react-mosaic/issues/162#issuecomment-1194558777 *
       */}
-      <DndProvider options={HTML5toTouch}>
-        <Component {...pageProps} />
-      </DndProvider>
-    </SessionProvider>
+        <DndProvider options={HTML5toTouch}>
+          <Component {...pageProps} />
+        </DndProvider>
+      </SessionProvider>
+      <Analytics />
+    </>
   )
 }
