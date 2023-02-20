ed8be6a8fe99f20b36a8253df73577af79e6c90e
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-19 19:41:19 -0500

    Update messages and design

--- src/components/SaveLoadLayoutMenu.jsx
+++ src/components/SaveLoadLayoutMenu.jsx
@@ -1,6 +1,6 @@
 import { DesktopContext } from '@/context/DesktopContext'
 import SaveIcon from '@/icons/save.svg'
-import { ExclamationTriangleIcon, FolderOpenIcon, TrashIcon, InformationCircleIcon } from '@heroicons/react/20/solid'
+import { ExclamationTriangleIcon, FolderOpenIcon, InformationCircleIcon, TrashIcon } from '@heroicons/react/20/solid'
 import { FolderArrowDownIcon } from '@heroicons/react/24/solid'
 import * as Popover from '@radix-ui/react-popover'
 import classNames from 'classnames'
@@ -15,12 +15,15 @@ function SaveLoadLayout () {
     desktopCtx.saveLayout(newSaveName)
     setOpen(false)
     setNewSaveName('')
+
+    
   }
 
   const handleRestore = layoutId => {
     desktopCtx.setDialog({
       isOpen: true,
-      message: `Are you sure you want to load this saved layout?`,
+      title: `Are you sure you want to load ${desktopCtx.savedLayouts[layoutId].name}?`,
+      message: `This will replace your current layout`,
       onConfirm: () => {
         desktopCtx.restoreLayout(layoutId)
         setOpen(false)
@@ -31,7 +34,8 @@ function SaveLoadLayout () {
   const handleDelete = layoutId => {
     desktopCtx.setDialog({
       isOpen: true,
-      message: `Are you sure you want to delete this saved layout?`,
+      title: `Are you sure you want to delete ${desktopCtx.savedLayouts[layoutId].name}?`,
+      message: `This is not undoable`,
       onConfirm: () => {
         desktopCtx.removeSavedLayout(layoutId)
       }
@@ -41,33 +45,35 @@ function SaveLoadLayout () {
   const renderSavedLayouts = () => {
     if (desktopCtx.savedLayouts && Object.keys(desktopCtx.savedLayouts).length) {
       return (
-        <ul className='overflow-y-auto border-2 divide-y rounded-md bg-background border-zinc-600 divide-zinc-600 max-h-72 scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-track-zinc-700 hover:scrollbar-thumb-zinc-400'>
-          {Object.entries(desktopCtx.savedLayouts)
-            .reverse()
-            .map(([key, value]) => (
-              <li key={key} className='grid grid-cols-[1fr_min-content] px-2 py-1 hover:bg-secondary/50 group'>
-                <div className='truncate'>{value.name}</div>
-                <div className='hidden gap-x-2 group-hover:flex'>
-                  <button
-                    type='button'
-                    onClick={() => handleRestore(key)}
-                    title='Load Saved Layout'
-                    className='hover:text-secondary'
-                  >
-                    <FolderOpenIcon className='w-5 h-5' />
-                  </button>
-                  <button
-                    type='button'
-                    onClick={() => handleDelete(key)}
-                    title='Delete Saved Layout'
-                    className='hover:text-secondary'
-                  >
-                    <TrashIcon className='w-5 h-5' />
-                  </button>
-                </div>
-              </li>
-            ))}
-        </ul>
+        <div className='overflow-y-auto border-2 rounded border-zinc-600'>
+          <ul className='divide-y bg-background divide-zinc-600 max-h-72 scrollbar-thin scrollbar-thumb-zinc-500 scrollbar-track-zinc-700 hover:scrollbar-thumb-zinc-400'>
+            {Object.entries(desktopCtx.savedLayouts)
+              .reverse()
+              .map(([key, value]) => (
+                <li key={key} className='grid grid-cols-[1fr_min-content] px-2 py-1 hover:bg-secondary/50 group'>
+                  <div className='truncate'>{value.name}</div>
+                  <div className='hidden gap-x-2 group-hover:flex'>
+                    <button
+                      type='button'
+                      onClick={() => handleRestore(key)}
+                      title='Load Saved Layout'
+                      className='hover:text-secondary'
+                    >
+                      <FolderOpenIcon className='w-5 h-5' />
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => handleDelete(key)}
+                      title='Delete Saved Layout'
+                      className='hover:text-secondary'
+                    >
+                      <TrashIcon className='w-5 h-5' />
+                    </button>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
       )
     } else {
       return <div className='mt-4 text-sm text-center opacity-50'>No saved layouts</div>
@@ -85,7 +91,7 @@ function SaveLoadLayout () {
         className='z-20 p-4 mx-4 text-left border-2 rounded-md bg-zinc-900 text-zinc-400 border-zinc-600 w-72 drop-shadow-panel'
       >
         <div>
-          <h2 className="font-semibold">Save Layout</h2>
+          <h2 className='font-semibold'>Save Layout</h2>
 
           <div
             className={classNames('flex mt-2 gap-x-3 items-center', {
@@ -100,6 +106,7 @@ function SaveLoadLayout () {
                 setNewSaveName(event.target.value)
               }}
               disabled={!Object.keys(desktopCtx.channels).length}
+              required="true"
             />
 
             <button className='hover:text-secondary' onClick={handleSave} title='Save Layout'>
@@ -109,8 +116,8 @@ function SaveLoadLayout () {
 
           {!Object.keys(desktopCtx.channels).length && (
             <p className='mt-2 text-sm opacity-50'>
-              <InformationCircleIcon className='inline w-4 h-4 mr-1 align-text-bottom' />
-              Add channels to layout to save
+              <InformationCircleIcon className='inline w-4 h-4 mr-2 align-text-bottom' />
+              Load channels to enable saving
             </p>
           )}
         </div>
@@ -124,10 +131,10 @@ function SaveLoadLayout () {
 
         <div className='my-5 border border-zinc-600'></div>
 
-        <p className='text-sm'>
-          <ExclamationTriangleIcon className='inline w-4 h-4 mr-1 align-text-bottom' />
-          Saved layouts are saved in local browser storage
-        </p>
+        <div className='grid grid-cols-[min-content_1fr] gap-x-2'>
+          <ExclamationTriangleIcon className='w-4 h-4 top-[2px] relative' />
+          <p className='text-sm'>Saved layouts are currently saved in local browser storage</p>
+        </div>
         <Popover.Arrow className='text-zinc-600' fill='currentColor' />
       </Popover.Content>
     </Popover.Root>
