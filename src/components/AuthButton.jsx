b3c145df017bad6121c6c86ee3fd2f8c49c631dd
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-02-22 10:34:39 -0500

    Add spinner to welcome and adjust color and layout (#20)

--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -1,5 +1,6 @@
 import { useSession, signIn } from 'next-auth/react'
 import ArenaMark from '@/icons/arena-mark.svg'
+import Spinner from './Spinner'
 
 export default function AuthButton () {
   const session = useSession() || {}
@@ -8,16 +9,21 @@ export default function AuthButton () {
   const loading = status === 'loading'
 
   if (loading) {
-    return <div>Loading...</div>
+    return (
+      <div className='h-14'>
+        <Spinner className='w-12 h-12' />
+        <div className='mt-2 text-sm text-zinc-600'>Loading</div>
+      </div>
+    )
   }
 
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-14'>
       {!data && (
         <>
           <button
             onClick={() => signIn('arena')}
-            className='border-2 rounded-lg border-primary text-primary py-2 px-4 flex items-center gap-x-2 bg-primary/10 hover:bg-secondary/20 hover:text-secondary hover:border-secondary'
+            className='flex items-center px-4 py-2 border-2 rounded-lg border-zinc-600 text-zinc-300 gap-x-2 bg-zinc-900 hover:bg-secondary/20 hover:text-secondary hover:border-secondary'
           >
             <ArenaMark className='w-8' />
             Sign in with Are.na
