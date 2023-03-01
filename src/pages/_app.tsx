b216583329843c21d3e25efae4ad2075bcdb7bed
Michael Guidetti <mguidetti@users.noreply.github.com>
2023-03-01 12:21:26 -0500

    Move comment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,13 @@ import HTML5toTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
 import 'react-mosaic-component/react-mosaic-component.css'
 import '../styles/globals.css'
 
+// DndProvider included to fix https://github.com/nomcopter/react-mosaic/issues/162
+// Fix from https://github.com/nomcopter/react-mosaic/issues/162#issuecomment-1194558777 *
+
 export default function App ({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <SessionProvider session={session}>
-        {/*
-        DndProvider included to fix https://github.com/nomcopter/react-mosaic/issues/162
-        Fix from https://github.com/nomcopter/react-mosaic/issues/162#issuecomment-1194558777 *
-      */}
         <DndProvider options={HTML5toTouch}>
           <Component {...pageProps} />
         </DndProvider>
