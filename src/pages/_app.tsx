e5a0d9b2dce458a1acdc5f5f2e1583a9cbc752ea
Michael Guidetti <mguidetti@users.noreply.github.com>
2026-03-28 16:28:23 -0400

    Remove old workaround dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,14 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
-import { DndProvider } from 'react-dnd-multi-backend'
-import { HTML5toTouch } from 'rdndmb-html5-to-touch'
 import 'react-mosaic-component/react-mosaic-component.css'
 import '../styles/globals.css'
-
-// DndProvider included to fix https://github.com/nomcopter/react-mosaic/issues/162
-// Fix from https://github.com/nomcopter/react-mosaic/issues/162#issuecomment-1194558777 *
 
 export default function App ({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <SessionProvider session={session}>
-        <DndProvider options={HTML5toTouch}>
-          <Component {...pageProps} />
-        </DndProvider>
+        <Component {...pageProps} />
       </SessionProvider>
       <Analytics />
     </>
